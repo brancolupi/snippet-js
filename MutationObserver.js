c43adb1227e8178b0ae62191849f3d2198743563
Lucio Asciolla <93661365+brancolupi@users.noreply.github.com>
2024-04-03 15:09:29 +0200

    Update MutationObserver.js

--- MutationObserver.js
+++ MutationObserver.js
@@ -2,46 +2,46 @@
 
 var targetElement = document.getElementById('elementId');
   
-  var observer = new MutationObserver(function(mutations) {
+var observer = new MutationObserver(function(mutations) {
   
-  console.log('Mutazione rilevata');
+console.log('Mutazione rilevata');
   
-  mutations.forEach(function(mutation) {
+mutations.forEach(function(mutation) {
     console.log('Tipo di mutazione:', mutation.type);
     console.log('Elemento bersaglio:', mutation.target);
     console.log('Nuovi nodi:', mutation.addedNodes);
     console.log('Nodi rimossi:', mutation.removedNodes);
   
-    // Controlla se il cambiamento riguarda l'innerHTML dell'elemento
+// Controlla se il cambiamento riguarda l'innerHTML dell'elemento
     if (mutation.type === 'childList' && mutation.target === targetElement) {
       console.log('Il contenuto interno dell\'elemento è cambiato.');
       console.log('Nuovo contenuto:', targetElement.innerHTML);
-      // Puoi eseguire qui le azioni desiderate quando l'innerHTML cambia
+// Puoi eseguire qui le azioni desiderate quando l'innerHTML cambia
     }
     
   });
 
-	// Avvia l'osservatore con l'elemento target e le opzioni
-	observer.observe(targetElement, config);  
+// Avvia l'osservatore con l'elemento target e le opzioni
+observer.observe(targetElement, config);  
 
 //////////////////// Exemple 2 ////////////////////
   
 
-  // Seleziona l'elemento da osservare
-  var targetElement = document.getElementById('elementId');
-	// Crea un nuovo MutationObserver
-	var observer = new MutationObserver(function(mutations) {
+// Seleziona l'elemento da osservare
+var targetElement = document.getElementById('elementId');
+// Crea un nuovo MutationObserver
+var observer = new MutationObserver(function(mutations) {
 	mutations.forEach(function(mutation) {
 		// Controlla se il cambiamento riguarda l'innerHTML dell'elemento
 		if (mutation.type === 'childList' && mutation.target === targetElement) {
-    console.log('Il contenuto interno dell\'elemento è cambiato.');
+		console.log('Il contenuto interno dell\'elemento è cambiato.');
 		// Puoi eseguire qui le azioni desiderate quando l'innerHTML cambia
 		}
 	});
-	});
+});
 
-	// Configura le opzioni per l'osservatore
-	var config = { childList: true, subtree: true };
+// Configura le opzioni per l'osservatore
+var config = { childList: true, subtree: true };
 
-	// Avvia l'osservatore con l'elemento target e le opzioni
-	observer.observe(targetElement, config);
+// Avvia l'osservatore con l'elemento target e le opzioni
+observer.observe(targetElement, config);
